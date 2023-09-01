f491ecf1ac339da0634e39137cafcc0cde0e0bf0
khanakitclass <khanakitclass@gmail.com>
2023-09-01 09:45:41 +0530

    intro

---
+++ js/intro.js
@@ -0,0 +1,9 @@
+// debugg
+console.log("Hello World!");
+
+document.write("<h1>BitKhanan IT Education</h1>")
+
+alert("OK");
+
+let x = parseInt(prompt("Please enter value of x: ", 0));
+console.log(x);
